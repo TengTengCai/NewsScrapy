4e6e1a9945a6c1f8776a5628e0d0c1d806665d73
tianjun <1021766585@qq.com>
2018-07-19 16:34:29 +0800

    腾讯爬虫

--- news/settings.py
+++ news/settings.py
@@ -52,9 +52,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'news.middlewares.SeleniumMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   'news.middlewares.SeleniumMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
